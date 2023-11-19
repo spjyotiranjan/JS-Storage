b211bf8f388c600addb664ece4d987811aad04d1
spjyotiranjan <142651812+spjyotiranjan@users.noreply.github.com>
2023-11-19 22:05:25 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+//Challenge 1: Store the user's first name, last name, age, country, and state in your browser's localStorage using the setItem() method and print the localStorage in the console.
+
+//Challenge 2: Get the users data stored on your browser's localStorage using the getItem() method and print the localStorage in the console.
+
+//Challenge 3: Using removeItem() method, remove the "state" data stored in your browser's localStorage and print the localStorage in the console.
+
+//Challenge 4: Using clear() method to remove all the data stored in your browser's localStorage and print the localStorage in the console.
+
+//Challenge 5: Using JSON.stringify() convert the given object into the string and print the same.
+const user = {
+  firstName: 'Rajat',
+  age: 25,
+  skills: ['HTML', 'CSS', 'JS', 'React'],
+};
